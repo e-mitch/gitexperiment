7b638e5369eb1cae527d6262ddf9ed0ad7f44a7e
boromirthebold@gmail.com <boromirthebold@gmail.com>
2021-10-25 11:51:48 -0400

    Version 2

--- Main.java
+++ Main.java
@@ -5,19 +5,4 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Commentarius");
-        primaryStage.setScene(new Scene(root, 1000, 800));
-        primaryStage.show();
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-}
+//"version 2"
